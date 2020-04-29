b28cdcfb94b3fe4a4acf870349da7c492d8efbf8
unknown <khomich10@mail.ru>
2020-04-29 21:55:25 +0300

    restuctering app les2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader = () => {
-    return <h1>Todo List</h1>;
-}
+import AppHeader from './components/AppHeader';
+import SearchBlock from './components/SearchBlock';
+import TodoList from './components/TodoList';
 
-const SearchBlock = () => {
-    return <input placeholder="search" />;
-}
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>learn JS</li>
-            <li>learn React</li>
-        </ul>
+
+
+const App = () => {
+    return(
+        <div>
+            <AppHeader />
+            <SearchBlock />
+            <TodoList />
+        </div>
     );
 }
 
-const el = (
-    <div>
-        <AppHeader />
-        <SearchBlock />
-        <TodoList />
-    </div>
-);
 
-ReactDOM.render(el, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
