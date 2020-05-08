9dac12c64db5d94d831e30df21f6aaa72836fc6e
unknown <khomich10@mail.ru>
2020-05-08 22:01:25 +0300

    end refactoring project/ add new components/make class components

--- src/components/SearchBlock/SearchBlock.js
+++ src/components/SearchBlock/SearchBlock.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './SearchBlock.css';
+import ItemStatusFilter from '../ItemStatusFilter';
 
 const SearchBlock = () => {
     const searchBlockText = 'What are you want to search?'
     return (
         <div className="search-block">
             <input placeholder={searchBlockText} />
+            <ItemStatusFilter/>
         </div>
    );
 }
