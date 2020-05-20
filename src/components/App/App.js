e83b988447e6d2c3fc97efc64d50c6c42a6823aa
unknown <khomich10@mail.ru>
2020-05-20 22:02:48 +0300

    start search and addItem

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,8 +16,11 @@ class App extends React.Component {
             { label: 'Learn HTML', important: false, done: false, id:1, },
             { label: 'Learn JS', important: false, done: false, id:2, },
             { label: 'Learn React', important: false, done: false, id:3, },
-        ]
+        ],
+        detectedText: '',
     };
+
+    
 
     deleteItem = (id) =>{
         this.setState((prevState ) => {
@@ -51,49 +54,51 @@ class App extends React.Component {
         });
     }
 
-    toggleDone = (id) => {
-        this.setState(( {todoData} ) => {
-            const index = todoData.findIndex((elem) => elem.id === id);
+    toggleProp = (id, arr, prop) => {
+        const index = arr.findIndex((elem) => elem.id === id);
             
-            const oldObj = todoData[index];
-            const newOjc = {...oldObj, done: !oldObj.done};
-           
-            const newArr = [
-                ...todoData.slice(0,index), 
-                newOjc,
-                ...todoData.slice(index + 1)
-            ];
+        const oldObj = arr[index];
+        const newObj = {...oldObj, [prop]: !oldObj[prop]};
+        
+        return [
+            ...arr.slice(0,index), 
+            newObj,
+            ...arr.slice(index + 1)
+        ];
     
+    }
+
+    toggleDone = (id) => {
+        this.setState(({ todoData }) =>{
             return {
-                todoData: newArr
-            }
+                todoData: this.toggleProp(id, todoData, 'done')
+            };
         });
     } 
 
     toggleImportant = (id) => {
-        console.log('id')
-        this.setState(( {todoData} ) => {
-            const index = todoData.findIndex((elem) => elem.id === id);
-            
-            const oldObj = todoData[index];
-            const newOjc = {...oldObj, important: !oldObj.important};
-           
-            const newArr = [
-                ...todoData.slice(0,index), 
-                newOjc,
-                ...todoData.slice(index + 1)
-            ];
-    
+        this.setState(({ todoData }) =>{
             return {
-                todoData: newArr
+                todoData: this.toggleProp(id, todoData, 'important')
             }
         });
     } 
     
+    search = (arr, detectedText) => {
+        if(detectedText.length === 0){
+            return arr;
+        }
+        return arr.filter((el) => {
+            return el.label.indexOf(detectedText) > -1;
+        });
+    }
+
     render() {
 
-        const done = this.state.todoData.filter((el) => el.done).length;
-        const todo = this.state.todoData.filter((el) => !el.done).length;
+        const { todoData, detectedText } = this.state;
+        const detectedItems = this.search(todoData, detectedText );
+        const done = todoData.filter((el) => el.done).length;
+        const todo = todoData.filter((el) => !el.done).length;
         
 
         return(
@@ -101,7 +106,7 @@ class App extends React.Component {
                 <AppHeader toDo={todo}  done={done}/>
                 <SearchBlock />
                 <TodoList 
-                    todoItems={this.state.todoData} 
+                    todoItems={ detectedItems } 
                     onDelete={ this.deleteItem}
                     onToggleImportant={ this.toggleImportant }
                     onToggleDone={ this.toggleDone }
